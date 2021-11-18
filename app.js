e9720956d85c92014b84a8c9b1e5f42109fa79c8
NikhilDugaje <nikhil.dugaje@bnedloudcloud.com>
2021-11-18 22:29:31 +0530

    Uploading node-app

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+let ip_add = require("ip");
+
+app.get('/', (req, res) => {
+    let obj = {
+        msg: 'Fantastic!!! You did it... Very Good & Keep it up...',
+        server_ip: ip_add.address()
+    }
+    console.log(obj)
+    res.send(obj)
+})
+
+app.get('/service-a', (req, res) => {
+    let obj = {
+        msg: 'Nice!!! You did it... Service A is responding...',
+        server_ip: ip_add.address()
+    }
+    console.log(obj)
+    res.send(obj)
+})
+
+app.get('/service-b', (req, res) => {
+    let obj = {
+        msg: 'Good!!! You did it... Service B is responding...',
+        server_ip: ip_add.address()
+    }
+    console.log(obj)
+    res.send(obj)
+})
+
+const port = 80
+app.listen(port, () => {
+    console.log(`Example app listening at ${port}`)
+})
